89f81131cee4b24f57d70e8da6b82ee216537550
Devin <devinpquinn4@gmail.com>
2026-08-21 18:09:10 -0400

    interim

---
+++ Assets/Scripts/WidthSetter.cs
@@ -0,0 +1,33 @@
+using TMPro;
+using UnityEngine;
+
+[ExecuteAlways]
+[RequireComponent(typeof(RectTransform))]
+[RequireComponent(typeof(TMP_Text))]
+public class WidthSetter : MonoBehaviour
+{
+    [SerializeField] private float maxWidth = 500f;
+
+    private RectTransform rectTransform;
+    private TMP_Text text;
+    private float lastPreferredWidth = -1f;
+
+    void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        text = GetComponent<TMP_Text>();
+    }
+
+    void Update()
+    {
+        // Measured unconstrained so the rect's own width doesn't feed back into the value.
+        float preferredWidth = text.GetPreferredValues(Mathf.Infinity, Mathf.Infinity).x;
+        if (Mathf.Approximately(preferredWidth, lastPreferredWidth))
+            return;
+
+        lastPreferredWidth = preferredWidth;
+        rectTransform.SetSizeWithCurrentAnchors(
+            RectTransform.Axis.Horizontal,
+            Mathf.Min(preferredWidth, maxWidth));
+    }
+}
