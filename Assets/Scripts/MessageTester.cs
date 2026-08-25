4140dfc0b7ec00628eb5cda91fd681d6e03577d6
Devin <devinpquinn4@gmail.com>
2026-08-24 21:57:54 -0400

    interim

---
+++ Assets/Scripts/MessageTester.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+public class MessageTester : MonoBehaviour
+{
+    public MessageManager messageManager;
+    public List<string> messages;
+    public float messageInterval = 2f;
+    
+    void Start()
+    {
+        StartCoroutine(AddMessages());
+    }
+    
+    IEnumerator AddMessages()
+    {
+        yield return new WaitForSeconds(1f); // Wait for 1 second before starting to add messages
+        
+        foreach (string message in messages)
+        {
+            messageManager.AddMessage(message);
+            yield return new WaitForSeconds(messageInterval);
+        }
+    }
+}
