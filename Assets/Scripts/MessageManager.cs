8a6be5afc6c212478fa7bd8462b3a7b8f069aa7b
Devin <devinpquinn4@gmail.com>
2026-08-24 21:51:52 -0400

    interim

--- Assets/Scripts/MessageManager.cs
+++ Assets/Scripts/MessageManager.cs
@@ -1,9 +1,28 @@
 using UnityEngine;
 using UnityEngine.UI;
-using System.Collections.Generic;
 
 public class MessageManager : MonoBehaviour
 {
     public GameObject messagePrefab;
     public Transform messageContainer;
+
+    private MessageHandler currentMessage;
+    private string currentMessageText;
+
+    public MessageHandler AddMessage(string message)
+    {
+        if (currentMessage != null)
+        {
+            currentMessage.SetOld(currentMessageText);
+        }
+
+        GameObject instance = Instantiate(messagePrefab, messageContainer);
+        MessageHandler handler = instance.GetComponent<MessageHandler>();
+        handler.SetMessage(message);
+
+        currentMessage = handler;
+        currentMessageText = message;
+
+        return handler;
+    }
 }
