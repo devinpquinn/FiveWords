9ce8fa129675a6838994e8ff6a0c92049a1476ff
Devin <devinpquinn4@gmail.com>
2026-08-27 15:34:02 -0400

    interim

--- Assets/Scripts/MessageManager.cs
+++ Assets/Scripts/MessageManager.cs
@@ -1,16 +1,40 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
 public class MessageManager : MonoBehaviour
 {
     public GameObject messagePrefab;
     public Transform messageContainer;
+    public GameObject typingIndicator;
+    public float typingDuration = 1f;
 
     private MessageHandler currentMessage;
     private string currentMessageText;
 
-    public MessageHandler AddMessage(string message)
+    void Awake()
     {
+        if (typingIndicator != null)
+        {
+            typingIndicator.SetActive(false);
+        }
+    }
+
+    public void AddMessage(string message)
+    {
+        StartCoroutine(AddMessageRoutine(message));
+    }
+
+    private IEnumerator AddMessageRoutine(string message)
+    {
+        if (typingIndicator != null)
+        {
+            typingIndicator.transform.SetAsLastSibling();
+            typingIndicator.SetActive(true);
+            yield return new WaitForSeconds(typingDuration);
+            typingIndicator.SetActive(false);
+        }
+
         if (currentMessage != null)
         {
             currentMessage.SetOld(currentMessageText);
@@ -22,7 +46,5 @@ public class MessageManager : MonoBehaviour
 
         currentMessage = handler;
         currentMessageText = message;
-
-        return handler;
     }
 }
